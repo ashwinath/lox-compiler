5e0bb156466ea1e4c7c33d34f5d3725590492c1a
Ashwin Chatterji <ashwinath@hotmail.com>
2021-07-10 14:06:28 +0800

    11. Resolving and Binding

--- java/src/com/ashwinchat/jlox/Environment.java
+++ java/src/com/ashwinchat/jlox/Environment.java
@@ -19,6 +19,14 @@ public class Environment {
         this.enclosing = enclosing;
     }
 
+    Environment ancestor(int distance) {
+        Environment environment = this;
+        for (int i = 0; i < distance; i++) {
+            environment = environment.enclosing;
+        }
+        return environment;
+    }
+
     Object get(Token name) {
         if (this.values.containsKey(name.lexeme)) {
             return values.get(name.lexeme);
@@ -40,5 +48,13 @@ public class Environment {
             return;
         }
         throw new RuntimeError(name, "Undefined variable ' " + name.lexeme + "'.");
+    }
+
+    Object getAt(int distance, String name) {
+        return this.ancestor(distance).values.get(name);
+    }
+
+    void assignAt(int distance, Token name, Object value) {
+        this.ancestor(distance).values.put(name.lexeme, value);
     }
 }
--- java/src/com/ashwinchat/jlox/Interpreter.java
+++ java/src/com/ashwinchat/jlox/Interpreter.java
@@ -1,11 +1,14 @@
 package com.ashwinchat.jlox;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class Interpreter implements Expr.Visitor<Object>, Stmt.Visitor<Void> {
     final Environment globals = new Environment();
     private Environment environment = globals;
+    private final Map<Expr, Integer> locals = new HashMap<>();
 
     private static class BreakException extends RuntimeException {}
 
@@ -31,7 +34,14 @@ public class Interpreter implements Expr.Visitor<Object>, Stmt.Visitor<Void> {
     @Override
     public Object visitAssignExpr(Expr.Assign expr) {
         Object value = this.evaluate(expr.value);
-        environment.assign(expr.name, value);
+
+        Integer distance = this.locals.get(expr);
+        if (distance != null) {
+            this.environment.assignAt(distance, expr.name, value);
+        } else {
+            this.globals.assign(expr.name, value);
+        }
+
         return value;
     }
 
@@ -96,8 +106,12 @@ public class Interpreter implements Expr.Visitor<Object>, Stmt.Visitor<Void> {
                 return !this.isEqual(left, right);
             case EQUAL_EQUAL:
                 return this.isEqual(left, right);
-            }
+        }
         return null;
+    }
+
+    void resolve(Expr expr, int depth) {
+        this.locals.put(expr, depth);
     }
 
     @Override
@@ -127,7 +141,16 @@ public class Interpreter implements Expr.Visitor<Object>, Stmt.Visitor<Void> {
 
     @Override
     public Object visitVariableExpr(Expr.Variable expr) {
-        return environment.get(expr.name);
+        return this.lookUpVariable(expr.name, expr);
+    }
+
+    private Object lookUpVariable(Token name, Expr expr) {
+        Integer distance = this.locals.get(expr);
+        if (distance != null) {
+            return this.environment.getAt(distance, name.lexeme);
+        } else {
+            return this.globals.get(name);
+        }
     }
 
     private boolean truthy(Object object) {
