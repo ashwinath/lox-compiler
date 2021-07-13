039de5e9e7e67e81e27d8f19cf2215cfc793a7cf
Ashwin Chatterji <ashwinath@hotmail.com>
2021-07-13 09:34:20 +0000

    15. A Virtual Machine.

--- c/src/main.c
+++ c/src/main.c
@@ -1,19 +1,41 @@
 #include "common.h"
 #include "chunk.h"
 #include "debug.h"
+#include "vm.h"
 
 int main(int argc, const char *argv[]) {
+    initVM();
+
     Chunk chunk;
     initChunk(&chunk);
 
-    // hand compile some actions
+    // store 1.2 as constant
     int constant = addConstant(&chunk, 1.2);
     writeChunk(&chunk, OP_CONSTANT, 123);
     writeChunk(&chunk, constant, 123);
-    // end hand compile some actions
+
+    // store 3.4 as constant
+    constant = addConstant(&chunk, 3.4);
+    writeChunk(&chunk, OP_CONSTANT, 123);
+    writeChunk(&chunk, constant, 123);
+
+    // Add previous 2 numbers
+    writeChunk(&chunk, OP_ADD, 123);
+
+    // store 5.6 as constant
+    constant = addConstant(&chunk, 5.6);
+    writeChunk(&chunk, OP_CONSTANT, 123);
+    writeChunk(&chunk, constant, 123);
+
+    // Divide previous 2 numbers
+    writeChunk(&chunk, OP_DIVIDE, 123);
+    // Negate previous number
+    writeChunk(&chunk, OP_NEGATE, 123);
 
     writeChunk(&chunk, OP_RETURN, 123);
     disassembleChunk(&chunk, "test chunk");
+    interpret(&chunk);
+    freeVM();
     freeChunk(&chunk);
     return 0;
 }
