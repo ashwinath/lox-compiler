0ec447bb2e47494aaf385b7a9898058c55b0b973
Ashwin Chatterji <ashwinath@hotmail.com>
2021-07-31 10:55:18 +0000

    30. Optimisation: NaN Boxing.

--- c/src/value.h
+++ c/src/value.h
@@ -1,7 +1,56 @@
 #ifndef clox_value_h
 #define clox_value_h
 
+#include <string.h>
+
 #include "common.h"
+
+typedef struct Obj Obj;
+typedef struct ObjString ObjString;
+
+#ifdef NAN_BOXING
+
+#define SIGN_BIT ((uint64_t)0x8000000000000000)
+// This is actually quiet NaN bits + Intel's Floating point indefinite value
+#define QNAN     ((uint64_t)0x7ffc000000000000)
+
+// LSB bits
+#define TAG_NIL   1 // 01
+#define TAG_FALSE 2 // 10
+#define TAG_TRUE  3 // 11
+
+typedef uint64_t Value;
+
+#define IS_BOOL(value)   (((value) | 1) == TRUE_VAL)
+#define IS_NIL(value)    ((value) == NIL_VAL)
+#define IS_NUMBER(value) (((value) & QNAN) != QNAN)
+#define IS_OBJ(value)    (((value) & (SIGN_BIT | QNAN)) == (SIGN_BIT | QNAN))
+
+#define AS_BOOL(value)   ((value) == TRUE_VAL)
+#define AS_NUMBER(value) valueToNum(value)
+#define AS_OBJ(value)    ((Obj*)(uintptr_t)((value) & ~(SIGN_BIT | QNAN)))
+
+#define BOOL_VAL(b)     ((b) ? TRUE_VAL : FALSE_VAL)
+#define FALSE_VAL       ((Value)(uint64_t)(QNAN | TAG_FALSE))
+#define TRUE_VAL        ((Value)(uint64_t)(QNAN | TAG_TRUE))
+#define NIL_VAL         ((Value)(uint64_t)(QNAN | TAG_NIL))
+#define NUMBER_VAL(num) numToValue(num)
+// Seems like just filling up first few bits with 1 and object pointer as the last 48 bits
+#define OBJ_VAL(obj)    (Value)(SIGN_BIT | QNAN | (uint64_t)(uintptr_t)(obj))
+
+static inline double valueToNum(Value value) {
+    double num;
+    memcpy(&num, &value, sizeof(Value));
+    return num;
+}
+
+static inline Value numToValue(double num) {
+    Value value;
+    memcpy(&value, &num, sizeof(double));
+    return value;
+}
+
+#else
 
 typedef enum {
     VAL_BOOL,
@@ -10,9 +59,10 @@ typedef enum {
     VAL_OBJ,
 } ValueType;
 
-typedef struct Obj Obj;
-typedef struct ObjString ObjString;
-
+// This is 16 bytes
+// type = 4 bytes
+// but 4 bytes padding to round it up nicely as a 16 byte struct
+// Obj is 8 bytes (64 bit pointer and 64 bit double)
 typedef struct {
     ValueType type;
     union {
@@ -35,6 +85,8 @@ typedef struct {
 #define NIL_VAL           ((Value){VAL_NIL, {.number = 0}})
 #define NUMBER_VAL(value) ((Value){VAL_NUMBER, {.number = value}})
 #define OBJ_VAL(value)    ((Value){VAL_OBJ, {.obj = (Obj*) value}})
+
+#endif
 
 typedef struct {
     int capacity;
